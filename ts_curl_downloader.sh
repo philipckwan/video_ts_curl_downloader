52e71436421231b1eddf7057e32c2776496cdd36
Philip Kwan <philipckwan@gmail.com>
2024-03-01 11:58:11 +0800

    add multi processes (split) mode

--- ts_curl_downloader.sh
+++ ts_curl_downloader.sh
@@ -2,21 +2,29 @@
 
 # use this script to download multiple ts files then combine into one
 # Example:
-# >./ts_curl_downloader.sh 0 200 curl 0
-# >./ts_curl_downloader.sh 201 500 curl 0
-# >./ts_curl_downloader.sh 501 1100 curl 3
-# >./ts_curl_downloader.sh 0 1100 cat 0
+# $ ./ts_curl_downloader.sh 0 200 curl 0
+# $ ./ts_curl_downloader.sh 201 500 curl 0
+# $ ./ts_curl_downloader.sh 501 1100 curl 3
+# $ ./ts_curl_downloader.sh 0 1100 cat 0
+#
+# this will be a master invocation of the program that will sprawn multiple (in this case 12) invocations of the same program in background
+# only supports for the 'curl' mode
+# Example:
+# $ ./ts_curl_downloader.sh 0 1200 curl 0 12
 #
 
 arg_start=$1
 arg_end=$2
 arg_mode=$3
 arg_padding=$4
+arg_split=$5
 
 do_curl=0
 do_cat=0
 do_clear=0
+do_split=0
 fmt="%00d"
+split_num=1
 
 clear_threshold=3000
 
@@ -66,8 +74,19 @@ function arguments_check {
     fmt="%04d"    
   else
     fmt="%00d"
+    arg_padding=0
   fi
   echo "arguments_check: fmt: [$fmt]"
+
+  if [ -z "$arg_split" ]
+  then 
+    do_split=0
+  else
+    do_split=1
+    split_num=$arg_split
+  fi
+  echo "arguments_check: do_split: [$do_split]; split_num: [$split_num]"
+
 }
 
 function do_clear {
@@ -93,6 +112,22 @@ function do_curl_mod {
   done
 }
 
+function do_curl_split {
+  let "total_files = $arg_end - $arg_start + 1"
+  let "num_files_per_batch = $total_files / $split_num"
+
+  for (( i=0; i<$split_num; i++ )); do \
+    let "this_batch_start = $i * $num_files_per_batch + $arg_start"
+    if ((i == split_num - 1)); then
+      let "this_batch_end = $arg_end"
+    else
+      let "this_batch_end = ($i + 1) * $num_files_per_batch + $arg_start - 1"
+    fi    
+    bash ts_curl_downloader.sh $this_batch_start $this_batch_end curl $arg_padding &
+  done
+
+}
+
 function do_curl {
   echo "do_curl: 1.0;"
   for (( i=$arg_start; i<=$arg_end; i++ )); do	\
@@ -105,6 +140,7 @@ function do_curl {
         continue
       fi
     fi
+
 curl 'https://abcde.com/video'$j'.ts' \
   --compressed \
   -o $i.ts
@@ -123,7 +159,13 @@ arguments_check
 
 if [ $do_curl -eq 1 ]
 then
-  do_curl
+  if [ $do_split -eq 1 ]
+  then
+    do_curl_split
+  else
+    do_curl
+  fi
+  
 fi
 
 if [ $do_cat -eq 1 ]
